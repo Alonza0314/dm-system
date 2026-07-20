93cf38fa774692a90feafb43b579bf1412e72595
Alonza0314 <alonza.tu@gmail.com>
2026-07-20 08:07:18 +0000

    test: get delete category

--- integration-test/goTest/apiCategory_test.go
+++ integration-test/goTest/apiCategory_test.go
@@ -121,9 +121,73 @@ func testGetCategories(t *testing.T) {
 }
 
 func testGetCategory(t *testing.T) {
+	for _, ct := range categories {
+		t.Run("Get category "+ct, func(t *testing.T) {
+			response, err := util.SendHttpRequest(BASE_URL+"/category/"+ct, http.MethodGet, header, nil)
+			if err != nil {
+				handleSendHttpError(t, err)
+			}
 
+			handleCheckStatusCode(t, http.StatusOK, response.StatusCode)
+
+			var responseGetCategory model.ResponseGetCategory
+			if err := json.Unmarshal(response.Body, &responseGetCategory); err != nil {
+				handleJsonUnmarshalError(t, err)
+			}
+
+			if responseGetCategory.Name != ct {
+				t.Fatalf("incorrect response category name, expected %s, got %s", ct, responseGetCategory.Name)
+			}
+
+			if responseGetCategory.IdleDevice != 0 {
+				t.Fatalf("init idle device is not 0 in category %s", ct)
+			}
+
+			if responseGetCategory.UsingDevice != 0 {
+				t.Fatalf("init using device is not 0 in category %s", ct)
+			}
+		})
+	}
 }
 
 func testDeleteCategory(t *testing.T) {
+	t.Run("Delete 1 2", func(t *testing.T) {
+		for _, ct := range categories {
+			response, err := util.SendHttpRequest(BASE_URL+"/category/"+ct, http.MethodDelete, header, nil)
+			if err != nil {
+				handleSendHttpError(t, err)
+			}
 
+			handleCheckStatusCode(t, http.StatusOK, response.StatusCode)
+		}
+	})
+
+	t.Run("Duplicate delete 1 2", func(t *testing.T) {
+		for _, ct := range categories {
+			response, err := util.SendHttpRequest(BASE_URL+"/category/"+ct, http.MethodDelete, header, nil)
+			if err != nil {
+				handleSendHttpError(t, err)
+			}
+
+			handleCheckStatusCode(t, http.StatusNotFound, response.StatusCode)
+		}
+	})
+
+	t.Run("Check category is empty", func(t *testing.T) {
+		response, err := util.SendHttpRequest(BASE_URL+"/category", http.MethodGet, header, nil)
+		if err != nil {
+			handleSendHttpError(t, err)
+		}
+
+		handleCheckStatusCode(t, http.StatusOK, response.StatusCode)
+
+		var responseGetCategories model.ResponseGetCategories
+		if err := json.Unmarshal(response.Body, &responseGetCategories); err != nil {
+			handleJsonUnmarshalError(t, err)
+		}
+
+		if len(responseGetCategories.Categories) != 0 {
+			t.Fatalf("failed to get categories with incorrect length, expected %d, got %d", len(responseGetCategories.Categories), 0)
+		}
+	})
 }
